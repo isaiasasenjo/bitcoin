e2a42c1611bc0de7315c39f4a26cd9c17fed1180
isaiasasenjo <isaiasasenjo@gmail.com>
2023-12-28 21:59:18 +0100

    probando con  index2 28-12

---
+++ backend/js/blocks.js
@@ -0,0 +1,24 @@
+// Número de variables que deseas crear
+var cantidadDeVariables = 10;
+let last_block = 2568526;
+
+// Bucle for para crear y asignar valores a las variables
+for (var i = 1; i <= cantidadDeVariables; i++) {
+    // Construir el nombre de la variable
+    var nombreVariable = "last_block" + i;
+
+    // Asignar un valor numérico a la variable
+    window[nombreVariable] = last_block -1; // Puedes asignar el valor que desees
+
+    // Mostrar el valor de la variable en la consola (opcional)
+    console.log(last_block1 + ": " + window[last_block1]);
+}
+
+// Acceder a las variables creadas
+console.log(last_block1); // Muestra el valor de numero1
+console.log(last_block2); // Muestra el valor de numero2
+console.log(last_block3); // Muestra el valor de numero3
+console.log(last_block4); // Muestra el valor de numero4
+
+
+
