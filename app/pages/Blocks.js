bf604d6f4e1d8e5d1c134d02b822c7c2b0a93095
isaiasasenjo <isaiasasenjo@gmail.com>
2023-12-31 18:36:35 +0100

    a las 18:36 hora del 31-12-2023

--- app/pages/Blocks.js
+++ app/pages/Blocks.js
@@ -1,34 +1,43 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
+import styles2 from "../styles/Styles2.module.css";
 import Menu2 from "./../components/Menu2";
 import Peu from "../components/Peu";
 //import logo from "./../logos/triangulo.jpg";
 
 export default function Blocks() {
+  let last_block = 2568543;
+  
   return (
     
     <main>
-      <div className={styles.container}>
+      <div className={styles2.container}>
         <Menu2 />
 
         <br></br>
 
-
-<script>
-let last_block = 2568543;
-let x = 45;
-
-document.write(`bbbbbbbbb`)
-</script>
-
-
-
-
-
-
-
-
+<p>Blocks of Chain</p>
+         <div className={styles2.cuadrado}>
+            <h3>
+              {last_block}
+            </h3>
+        </div>
+        <div className={styles2.cuadrado}>
+            <h3>
+              {last_block-1}
+            </h3>
+        </div>
+        <div className={styles2.cuadrado}>
+            <h3>
+              {last_block-2}
+            </h3>
+        </div>
+        <div className={styles2.cuadrado}>
+            <h3>
+              {last_block-3}
+            </h3>
+        </div>
 
         <Peu />
       </div>
