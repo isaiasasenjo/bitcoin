b91e5451b791636ef738178a7a3c475b1961b91f
isaiasasenjo <isaiasasenjo@gmail.com>
2024-01-09 20:24:48 +0100

    Ultima version. No conecta pero el proceso es correcto

--- backend/getBlockchain1.js
+++ backend/getBlockchain1.js
@@ -6,7 +6,7 @@ const Client = require("bitcoin-core");
 
 const app = express();
 var corsOptions = {
-  origin: '[http://localhost](http://localhost)',
+  origin: 'http://localhost:3000',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 
